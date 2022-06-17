56eb31a9ec5111ba5430a8c25e29aa08e32628ce
David Pigeau <dpigeau@luno.com>
2022-06-17 11:23:04 +0200

    write_sql command

--- looknice/cli.py
+++ looknice/cli.py
@@ -1,8 +1,10 @@
-import importlib.resources
+# import importlib.resources
 import click
 import sqlfluff
-import looknice.utils
-from looknice.utils import SQLFLUFF_CONFIG
+import looknice.looker_utils as looker
+import looknice.databricks_utils as databricks
+import looknice.api
+from looknice.looker_utils import SQLFLUFF_CONFIG
 import re
 
 error_string = "no derived table code found"
@@ -23,7 +25,7 @@ def cli():
 )
 def lint(path: str):
     """Lint the derived table code. Take a path to a view.lkml file"""
-    code = looknice.utils.get_code(path)
+    code = looker.get_code(path)
     if code:
         res = sqlfluff.lint(
             code,
@@ -41,9 +43,9 @@ def lint(path: str):
 )
 def parameters(path: str):
     """LookML special parameters contained in the derived table code. Take a path to a view.lkml file."""
-    code = looknice.utils.get_code(path)
+    code = looker.get_code(path)
     if code:
-        parameters = looknice.utils.get_parameters(code)
+        parameters = looker.get_parameters(code)
         click.echo(parameters)
     else:
         click.echo(error_string)
@@ -55,10 +57,10 @@ def parameters(path: str):
 )
 def fix(path: str):
     """Fix derived table code. Take a path to a view.lkml file."""
-    code = looknice.utils.get_code(path)
+    code = looker.get_code(path)
     if code:
-        parameters = looknice.utils.get_parameters(code)
-        sql_code = looknice.utils.convert_to_sql(
+        parameters = looker.get_parameters(code)
+        sql_code = looker.convert_lookml(
             lookml = code,
             params = parameters
         )
@@ -67,7 +69,7 @@ def fix(path: str):
             dialect = "ansi",
             config_path = SQLFLUFF_CONFIG
         )
-        res = looknice.utils.convert_to_lookml(
+        res = looker.convert_sql(
             sql = fixed_sql_code,
             params = parameters
         )
@@ -76,21 +78,20 @@ def fix(path: str):
         click.echo(error_string)
 
 
-@cli.command()
-def config_path():
-    """Returns the location of the config files"""
-    with importlib.resources.path("looknice", "config") as p:
-        click.echo(p)
+# @cli.command()
+# def config_path():
+#     """Returns the location of the config files"""
+#     with importlib.resources.path("looknice", "config") as p:
+#         click.echo(p)
 
-#dimensions
 @cli.command()
 @click.argument(
     "path",
     type = str
 )
-def miss_description(path: str):
+def get_missing_descriptions(path: str):
     """Returns a list of dimensions with missing description"""
-    dimensions = looknice.utils.get_dimensions(path)
+    dimensions = looker.get_dimensions(path)
     res = [d["name"] for d in dimensions if ~("description" in d.keys())]
     if res:
         click.echo("\nThe following dimensions are missing a description:")
@@ -113,8 +114,8 @@ def join_views(
     right_path: str,
 ):
     """Returns the list of primary keys and duplicate dimensions and measures"""
-    left_dims = looknice.utils.get_dimensions(left_path)
-    right_dims = looknice.utils.get_dimensions(right_path)
+    left_dims = looker.get_dimensions(left_path)
+    right_dims = looker.get_dimensions(right_path)
 
     dup_names = []
     left_pk = None
@@ -141,26 +142,24 @@ def join_views(
         click.echo(f'{right_pk["name"]}(right)')
 
     click.echo("\nFailure to use the primary keys in a join will result in a many-to-many join.")
-
-
     if dup_names:
         click.echo(f"\nThe following dimensions were found in both view: {dup_names}")
         click.echo("Please make sure they are not identical before including both.\n")
 
-@cli.command()
-@click.argument(
-    "path",
-    type = str
-) 
-def get_comments(path: str) -> str:
-    """Returns a dictionary of comments based on the databrick schema file. Takes a databricks schema file as argument"""
-    script = looknice.utils.get_schema(path)
-    schema = re.findall(r"\((.*?)\)", script)[0].split(",")
+# @cli.command()
+# @click.argument(
+#     "path",
+#     type = str
+# ) 
+# def get_comments(path: str) -> str:
+#     """Returns a dictionary of comments based on the databrick schema file. Takes a databricks schema file as argument"""
+#     script = databricks.get_sql_code(path)
+#     schema = re.findall(r"\((.*?)\)", script)[0].split(",")
     
-    for s in schema:
-        name = s.split()[0]
-        comment = re.findall(r"\"(.*?)\"", s)[0]
-        click.echo(f"\n{name}: {comment}")
+#     for s in schema:
+#         name = s.split()[0]
+#         comment = re.findall(r"\"(.*?)\"", s)[0]
+#         click.echo(f"\n{name}: {comment}")
 
 @cli.command()
 @click.argument(
@@ -168,16 +167,45 @@ def get_comments(path: str) -> str:
     type = str
 ) 
 def write_lkml_schema(path: str) -> None:
-    """Returns a dictionary of comments based on the databrick schema file"""
+    """Write the lookml code from the SQL script"""
+    script = databricks.get_sql_code(path)
+    columns = re.findall(r"\((.*?)\)", script)[0].split(",")
+    schema, table = re.findall("CREATE TABLE IF NOT EXISTS (.*?)\(", script)[0].split(".")
 
-    script = looknice.utils.get_schema(path)
-    schema = re.findall(r"\((.*?)\)", script)[0].split(",")
-    for s in schema:
-        name = s.split()[0]
-        type = s.split()[1]
-        comment = re.findall(r"\"(.*?)\"", s)[0]
-        looknice.utils.print_dimension(
-            name = name,
-            type = type,
-            comment = comment
+    click.echo(f"\n\nview: {table}{{\n\tdescription: <TODO: description>\n\tsql_table_name: {schema}.{table} ;;")
+    miss_descriptions = []
+    for c in columns:
+        name = c.split()[0]
+        type = c.split()[1]
+        try:
+            comment = re.findall(r"\"(.*?)\"", c)[0]
+        except IndexError:
+            comment = "<TODO: comment>"
+            miss_descriptions.append(name)
+
+        click.echo(
+            databricks.convert_sql_columns(
+                name = name,
+                type = type,
+                comment = comment
+            )
         )
+
+    if miss_descriptions:
+        click.echo(f"\n\n***ALERT***\nThe following dimensions are missing a description:\n{miss_descriptions}")
+
+@cli.command()
+@click.argument(
+    "path",
+    type = str
+)
+def write_sql(path: str) -> None:
+    """Write the SQL script from the LookML view file"""
+    s = "\n\nCREATE OR REPLACE VIEW <TODO: schema>.vw_<TODO: viewname> (\n"
+    s = s + looker.convert_lookml_dims(path)[:-2] + "\n)\n" #[:-2] to remove last comma
+    s = s + looknice.api.fix(path)[:-1] +";\n\nALTER VIEW <TODO: schema>.vw_<TODO: viewname> OWNER TO `super-users`"
+    click.echo(s)
+    
+    miss_descriptions = looknice.api.get_missing_descriptions(path)
+    if miss_descriptions:
+        click.echo(f"\n\n***ALERT***\nThe following dimensions are missing a description:\n{miss_descriptions}")
